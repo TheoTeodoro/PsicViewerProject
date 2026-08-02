54bd1226d6beecf9b736f3ed4ec6d04330d5ddc3
Theo Teodoro <theotn12@gmail.com>
2026-08-02 14:31:49 -0400

    fix:falha que deixava o texto na tela de login invisível consertado

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using MauiApp1.Views;
-
 namespace MauiApp1;
-
 public partial class App : Application
 {
 	public App(IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
-
+		UserAppTheme = AppTheme.Light;
 		var loginPage = serviceProvider.GetRequiredService<LoginPage>();
 		MainPage = new NavigationPage(loginPage);
 	}
